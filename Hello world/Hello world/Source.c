f3a3fca54734ec2f26a422f2a619df6cd04602d1
Carlos Peralta <carlos@jardiper.es>
2016-02-17 12:26:15 +0100

    Hello world v0.1
    First program code by perso

---
+++ Hello world/Hello world/Source.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+int main(){
+
+	system("color 0a");
+	printf("\t H E L L O  W O R L D \n\n");
+	printf("Hello my friends by Carlos \n");
+
+
+	getchar();
+	return 0;
+}
